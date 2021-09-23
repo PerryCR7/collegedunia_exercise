8aca17b5443d0d64ba53c90255e2738002ea327e
unknown <parvinderkc123@gmail.com>
2021-09-23 07:32:22 +0530

    filesupdated

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+// const port =  3000 
 const hbs = require('hbs')
 const fs = require('fs')
 const load_data = require('./utils/load_data')
 
 
 // Define paths for Express config
-const PublicDirectoryPath = path.join(__dirname, './public')
+const PublicDirectoryPath = path.join(__dirname,'./public')
 const viewsPath = path.join(__dirname, './views')
 
 
@@ -27,17 +28,21 @@ app.get('/index', (req, res) => {
    res.render('index')
 })
 
-app.get('/get_collegedunia_data', async (req, res) => {
-   const data = await load_data()
-   return res.send({
-      data: data['colleges']
-   })
 
-})
+
+app.get('/get_collegedunia_data', async(req, res) => {
+    const data = await load_data()
+    // console.log(data['colleges'].length)
+    return res.send({
+        data: data['colleges']
+      
+     })
+   
+ })
 
 app.get('*', (req, res) => {
    res.render('index')
 })
-app.listen(PORT, () => {
-   console.log('Server is up on port!' + PORT)
-})
+ app.listen(PORT, () => {
+     console.log('Server is up on port!' + PORT)
+ })
